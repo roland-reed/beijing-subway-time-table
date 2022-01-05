f28c3e62e5d82504ccb26d160be64e3597a1ec39
roland-reed <rolandreed.cn+github.com@gmail.com>
2022-01-05 21:55:47 +0800

    feat: cache first

--- next.config.js
+++ next.config.js
@@ -1,11 +1,25 @@
 const withSvgr = require('next-plugin-svgr');
 const withPWA = require('next-pwa');
+const cache = require('next-pwa/cache');
 
 /** @type {import('next').NextConfig} */
 module.exports = withPWA(withSvgr({
   reactStrictMode: true,
   pwa: {
     dest: 'public',
-    disable: process.env.NODE_ENV === 'development'
+    disable: process.env.NODE_ENV === 'development',
+    runtimeCaching: [
+      {
+        urlPattern: /\//,
+        handler: 'CacheFirst',
+        options: {
+          cacheName: 'homepage',
+          expiration: {
+            maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+          }
+        },
+      },
+      ...cache
+    ]
   }
 }));
